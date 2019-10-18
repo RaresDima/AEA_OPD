c352abc1b2722ddeaeececda2f882e4f85437e00
Rares Dima <rares.dima96@yahoo.com>
2019-10-18 18:01:50 +0300

    Model versions 1-4
    Signed-off-by: Rares Dima <rares.dima96@yahoo.com>

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import time
 import subprocess
 
 import numpy as np
@@ -24,13 +25,18 @@ def write_datafile(model_path: str, v: int, b: int, r: int, target_lambda: int) 
 MINIZINC_PATH = r'E:\Tools\MiniZinc\minizinc'
 
 SOLVER_NAME = 'Gecode'
-GECODE_PATH = r'E:\Tools\Gecode\bin\fzn-gecode'
-MODEL_PATH  = r'.\models\FinancialPortfolio.mzn'
+SOLVER_PATH = r'E:\Tools\Gecode\bin\fzn-gecode'
+SOLVER_THREADS = 6
+SOLVER_TIME_LIMIT_S = 10
+
+MODEL_PATH  = r'.\models\FinancialPortfolio-v4.mzn'
 
 V, B, R = 10, 37, 14
-TARGET_LAMBDA = 9
+TARGET_LAMBDA = 7
 
-###################
+RUN_COUNT = 100
+
+# ENVIRONMENT SETUP
 
 DATA_PATH = write_datafile(MODEL_PATH, V, B, R, TARGET_LAMBDA)
 
@@ -39,31 +45,74 @@ DATA_PATH = os.path.abspath(DATA_PATH)
 FZN_PATH = os.path.abspath(f'{os.path.splitext(MODEL_PATH)[0]}.fzn')
 OZN_PATH = os.path.abspath(f'{os.path.splitext(MODEL_PATH)[0]}.ozn')
 
-###################
+# PRINT INSTANCE DATA
 
-print(f'SOLVER   : {SOLVER_NAME} \n'
-      f'MODEL    : {MODEL_PATH}   \n'
-      f'DATA     : {DATA_PATH}\n'
-      f'FLATZINC : {FZN_PATH} \n'
-      f'OZN PATH : {OZN_PATH} \n'
+print(f'SOLVER    : {SOLVER_NAME} \n'
+      f'THREADS   : {SOLVER_THREADS} \n'
+      f'TIME LIMIT: {SOLVER_TIME_LIMIT_S} s \n'
+      f'\n'
+      f'MODEL   : {MODEL_PATH}   \n'
+      f'DATA    : {DATA_PATH}\n'
+      f'FLATZINC: {FZN_PATH} \n'
+      f'OZN PATH: {OZN_PATH} \n'
+      f'\n'
       f'DATA: V = {V} \n'
       f'      B = {B} \n'
       f'      R = {R} \n'
-      f'      LAMBDA <= {TARGET_LAMBDA} \n'
-      f'Compiling...')
+      f'      LAMBDA <= {TARGET_LAMBDA} \n')
 
-subprocess.check_call([MINIZINC_PATH, '--compile', '--solver', SOLVER_NAME, MODEL_PATH, DATA_PATH])
+# MODEL COMPILATION
 
-print('Done.')
+print('Model compiling...')
+subprocess.check_call([MINIZINC_PATH, '--compile', '--solver', SOLVER_NAME, '-O5', MODEL_PATH, DATA_PATH])
+print('Model compiled.')
+
+# SOLVING BENCHMARK
 
 print('Solving...')
-solve_output = subprocess.check_output([GECODE_PATH, FZN_PATH]).decode()
+
+last_good_output = '=====UNKNOWN====='
+
+times = []
+for i in range(RUN_COUNT):
+
+    start = time.clock()
+    solve_output = subprocess.check_output([SOLVER_PATH,
+                                            '-p', str(SOLVER_THREADS),
+                                            '-t', str(SOLVER_TIME_LIMIT_S * 1000),
+                                            FZN_PATH]).decode()
+    stop = time.clock()
+
+    if 'UNKNOWN' not in solve_output:
+        last_good_output = solve_output
+
+    times += [stop - start]
+
+    print(f'Solve {i+1:{len(str(RUN_COUNT))}}: {times[-1]} s')
+
+times = [t for t in times if t < SOLVER_TIME_LIMIT_S]
+
 print('Done')
+print('Avg. time  :', np.mean(times), 's')
+print('Median time:', np.median(times), 's')
 
-solve_result = np.array(solve_output[solve_output.find('[')+1:solve_output.find(']')].strip().split(', ')).reshape((V, B))
+# ENVIRONMENT TEARDOWN
+
+if 'UNKNOWN' not in last_good_output:
+    solve_result = (
+        np.array(
+            list(map(
+                int,
+                last_good_output[last_good_output.find('[')+1:
+                                 last_good_output.find(']')]
+                    .strip()
+                    .split(', ')
+            ))
+        ).reshape((V, B)))
+else:
+    solve_result = last_good_output
+
 print(solve_result)
-
-
 
 os.remove(DATA_PATH)
 os.remove(FZN_PATH)
